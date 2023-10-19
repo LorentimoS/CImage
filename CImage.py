398bf1c854fc8ef84f6b366520782eafe8f8b403
LorentimoS <volkov@sfedu.ru>
2023-10-19 15:29:12 +0300

    add contrast and brightness changing

--- CImage.py
+++ CImage.py
@@ -1,7 +1,30 @@
 from PIL import Image
 
+def change_contrast(img_in, contrast, brightness):
+        wid, hei = img_in.size
+        img_out = Image.new('L', (wid,hei))
+        for pixX in range(wid):
+            for pixY in range(hei):
+                pxl = int(contrast*img_in.getpixel((pixX,pixY))) + brightness
+                img_out.putpixel((pixX,pixY), pxl)
+
+        return img_out
+    
+
 class CImage:
     def __init__(self, filename):
         self.fname = filename
         with Image.open(self.fname) as self.img:
             self.img.load()
+
+        self.mode = self.img.mode
+
+    def change_contrast(self, contrast, brightness):
+        if self.mode == 'L':
+            self.img = change_contrast(self.img, contrast, brightness)
+        else:
+            r, g, b = self.img.split()
+            r = change_contrast(r, contrast, brightness)
+            g = change_contrast(g, contrast, brightness)
+            b = change_contrast(b, contrast, brightness)
+            self.img = Image.merge('RGB', (r,g,b))
